0d7bd45478e1a5ae7d534c7cb42ea54ea4169378
reeeverb <skydoessoftware@gmail.com>
2024-03-26 03:33:28 -0500

    append added

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Data Structures
 - [x] Linked List
 - [x] Direct Access Hash Table
-- [] Other Hash Tables
+- [ ] Other Hash Tables
 - [ ] Two Way Linked List
 - [ ] Stack
 - [ ] Heap
--- linked-list.cpp
+++ linked-list.cpp
@@ -5,17 +5,28 @@ using namespace std;
 
 struct node{
     char* value;
-    node* next_node; 
+    node* next_node = NULL;
+    node* tail;
     bool root = false;
     int size;
 
-    char* operator[](int index){
+    node* operator[](int index){
         assert(root && index >= 0  && index < size);
         node* current = this;
         for (int i = 0; i < index; i++){
             current = current->next_node;
         }
-        return current->value;
+        return current;
+    }
+
+    node* operator+=(node* new_list){
+        assert(new_list->root && root);
+        this->tail->next_node = new_list;
+        this->size += new_list->size;
+        this->tail = new_list->tail;
+        new_list -> root = false;
+        new_list -> size = 0;
+        return this;
     }
 };
 
@@ -23,7 +34,7 @@ node* create_node(node* prev, char* value, int size)
 {
     node* data = (node*) malloc(sizeof(node)); 
     data->value =  (char*) malloc(strlen(value)+1);
-    data->next_node = NULL;
+    data->next_node = (node*) malloc(sizeof(node*));
     data->root = true;
     data->size = size;
     strcpy(data->value,value);
@@ -40,18 +51,35 @@ node* create_node(node* prev, char* value)
     strcpy(data->value,value);
     if (prev != NULL)
         prev->next_node = data;
+    else{
+        data->root=true;
+        data->size=1;
+        data->tail=data;
+    }
     return data;
 }
 
-node* create_list(int size)
+node* create_list(int size, char* default_value)
 {
-    char data[] = "Noneee";
-    node* root = create_node(NULL,data,size);
+    char data[] = "None";
+    node* root = create_node(NULL,default_value,size);
     node* prev = root;
     for (int i = 1; i < size; i++){
-        data[i] = 'P';
-        node* current = create_node(prev,data);
+        node* current = create_node(prev,default_value);
         prev = current;
+        root->tail = current;
     }
     return root;
 }
+
+
+/*
+int main()
+{
+    node* root = create_list(5);
+    char new_value[] = "hello";
+    (*root)[1]->value = new_value;
+    cout << (*root)[0]->value << "\n";
+    cout << (*root)[1]->value << "\n";
+}
+*/
